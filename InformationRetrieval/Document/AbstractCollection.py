c033f6e3b5282cd6a05931679b77a863b9429201
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-04-04 11:51:10 +0300

    Added empty category case to loadCategories.

--- InformationRetrieval/Document/AbstractCollection.py
+++ InformationRetrieval/Document/AbstractCollection.py
@@ -63,8 +63,10 @@ class AbstractCollection:
         line = input_file.readline().strip()
         while line != "":
             items = line.split("\t")
-            doc_id = int(items[0])
-            self.documents[doc_id].setCategory(self.category_tree, items[1])
+            if len(items) > 0:
+                doc_id = int(items[0])
+                if len(items) > 1:
+                    self.documents[doc_id].setCategory(self.category_tree, items[1])
             line = input_file.readline().strip()
         input_file.close()
 
