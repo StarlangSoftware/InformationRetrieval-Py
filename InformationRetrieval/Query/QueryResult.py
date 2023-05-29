61ef8e5074998e2d68650e9d28677c82dfa376db
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-05-29 18:30:48 +0300

    Updated getBest method.

--- InformationRetrieval/Query/QueryResult.py
+++ InformationRetrieval/Query/QueryResult.py
@@ -39,13 +39,21 @@ class QueryResult:
         return result
 
     def getBest(self, K: int):
-        minHeap = MinHeap(2 * K, lambda x1, x2: -1 if x1.getScore() > x2.getScore() else (0 if x1.getScore() == x2.getScore() else 1))
-        for queryResultItem in self.__items:
-            minHeap.insert(queryResultItem)
+        minHeap = MinHeap(K, lambda x1, x2: 1 if x1.getScore() > x2.getScore() else (0 if x1.getScore() == x2.getScore() else -1))
+        i = 0
+        while i < K and i < len(self.__items):
+            minHeap.insert(self.__items[i])
+            i = i + 1
+        for i in range(K + 1, len(self.__items)):
+            top_item = minHeap.delete()
+            if isinstance(top_item, QueryResultItem) and top_item.getScore() > self.__items[i].getScore():
+                minHeap.insert(top_item)
+            else:
+                minHeap.insert(self.__items[i])
         self.__items.clear()
         i = 0
         while i < K and not minHeap.isEmpty():
-            self.__items.append(minHeap.delete())
+            self.__items.insert(0, minHeap.delete())
             i = i + 1
 
     def __repr__(self):
