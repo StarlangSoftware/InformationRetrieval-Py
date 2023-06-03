56cc4661ca79a27313b587611283aa5c537070e3
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-06-03 15:04:51 +0300

    Updated attributeSearch.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='NlpToolkit-InformationRetrieval',
-    version='1.0.5',
+    version='1.0.6',
     packages=['InformationRetrieval',
               'InformationRetrieval.Document',
               'InformationRetrieval.Index',
