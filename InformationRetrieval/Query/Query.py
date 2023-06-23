de4f67e0eae31c8fc819cd57cb6fa20cd1c121c7
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-06-23 10:18:00 +0300

    Updated attributeSearch, added shortcut search.

--- InformationRetrieval/Query/Query.py
+++ InformationRetrieval/Query/Query.py
@@ -1,10 +1,18 @@
 from __future__ import annotations
+
+import re
+
 from Dictionary.Word import Word
 
 
 class Query:
 
     __terms: [Word]
+    __shortcuts: list = ["cc", "cm2", "cm", "gb", "ghz", "gr", "gram", "hz", "inc", "inch", "inç", "kg", "kw", "kva",
+                         "litre", "lt", "m2", "m3", "mah", "mb", "metre", "mg", "mhz", "ml", "mm", "mp", "ms",
+                         "mt", "mv", "tb", "tl", "va", "volt", "watt", "ah", "hp", "oz", "rpm", "dpi", "ppm", "ohm",
+                         "kwh", "kcal", "kbit", "mbit", "gbit", "bit", "byte", "mbps", "gbps", "cm3", "mm2", "mm3",
+                         "khz", "ft", "db", "sn", "g", "v", "m", "l", "w", "s"]
 
     def __init__(self, query: str = None):
         self.__terms = []
@@ -29,6 +37,11 @@ class Query:
             if i < self.size() - 1:
                 pair = self.__terms[i].getName() + " " + self.__terms[i + 1].getName()
                 if pair in attributeList:
+                    phraseAttributes.__terms.append(Word(pair))
+                    i = i + 2
+                    continue
+                if self.__terms[i + 1].getName() in self.__shortcuts and re.fullmatch("[+-]?\\d+|[+-]?(\\d+)?\\.\\d*",
+                                                                                      self.__terms[i].getName()):
                     phraseAttributes.__terms.append(Word(pair))
                     i = i + 2
                     continue
